3bc033d34829337a287d59ce0da33ae3af784742
arafatdayan005 <arafatdayan005@gmail.com>
2023-08-19 17:58:02 +0600

    filter jobs category added

--- src/Components/CategoryCard.js
+++ src/Components/CategoryCard.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function CategoryCard({ category }) {
-    const {name, img, jobs} = category;
+function CategoryCard({ categories, handleCategory }) {
+    const {name, img, jobs, category} = categories;
     return (
-        <div className='bg-indigo-50 bg-opacity-50 p-10 rounded-lg text-center my-8'>
-            <img src={img} className='bg-indigo-100 bg-opacity-70 rounded-lg p-2 mx-auto' alt="" />
-            <p className='font-bold text-gray-600 pt-6'>{name}</p>
+        <div onClick={() => handleCategory(category)} className='bg-gradient-to-r from-[#7e8ffe1a] to-[#9873ff1a] p-10 rounded-lg text-center hover:scale-110 duration-300 hover:cursor-pointer'>
+            <img src={img} className='bg-gradient-to-r from-[#7e8ffe1a] to-[#9873ff1a] rounded-lg p-2 mx-auto' alt="" />
+            <p className='font-bold text-lg text-gray-600 pt-6'>{name}</p>
             <p className='text-gray-400 pt-2'>{jobs} Jobs Available</p>
         </div>  
     )
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import img from '../assets/All Images/P3OLGJ1 copy 1.png'
-import { useLoaderData } from 'react-router-dom'
 import CategoryCard from './CategoryCard'
 import JobDetailsCard from './JobDetailsCard'
 
 function Home() {
-  const jobDetails = useLoaderData()
   const [categoryList, setCategoryList] = useState([])
+  const [jobList, setJobList] = useState([])
+  const [filterCategory, setFilterCategory] = useState("")
   const [seeAll, setSeeAll] = useState(false)
 
   function seeAllCard() {
@@ -18,6 +18,24 @@ function Home() {
       .then(res => res.json())
       .then(data => setCategoryList(data))
   }, [])
+
+  useEffect(() => {
+    fetch("JobDetails.json")
+      .then(res => res.json())
+      .then(data => {
+        if (filterCategory === "") {
+          setJobList(data)
+        }
+        else {
+          const filterJobs = data.filter(job => job.category === filterCategory)
+          setJobList(filterJobs)
+        }
+      })
+  }, [filterCategory])
+
+  const handleCategory = (category) => {
+    setFilterCategory(category)
+  }
   return (
     <>
       <section className='bg-gradient-to-r from-[#7e8ffe1a] to-[#9873ff1a] pb-0 w-full'>
@@ -37,10 +55,10 @@ function Home() {
         <div>
           <h1 className='text-5xl font-bold text-center'>Job Category List</h1>
           <p className='text-gray-500 my-6 text-center'>Explore thousands of job opportunities with all the information you need. Its your future</p>
-          <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-4'>
+          <div className='grid md:grid-cols-2 lg:grid-cols-4 my-16 gap-8'>
             {
-              categoryList.map(category => (
-                <CategoryCard key={category.id} category={category}></CategoryCard>
+              categoryList.map(categories => (
+                <CategoryCard key={categories.id} categories={categories} handleCategory={handleCategory}></CategoryCard>
               ))
             }
           </div>
@@ -51,7 +69,7 @@ function Home() {
           <h1 className='text-5xl font-bold text-center'>Featured Job</h1>
           <div className='grid lg:grid-cols-2 gap-4 my-8'>
             {
-              jobDetails.slice(0, seeAll ? 6 : 4).map(job => (
+              jobList.slice(0, seeAll ? jobList.length : 6).map(job => (
                 <JobDetailsCard key={job.id} job={job}></JobDetailsCard>
               ))
             }
